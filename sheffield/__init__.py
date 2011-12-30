8ba0b8ee5f50db37fc55bf31fd60af636279c4ba
Dave King <dave@davbo.org>
2011-12-30 15:07:52 +0000

    Added constructor for building events from json

--- sheffield/__init__.py
+++ sheffield/__init__.py
@@ -1,5 +1,6 @@
-from mongoalchemy.document import Document
+from mongoalchemy.document import Document, Index
 from mongoalchemy import fields
+from utils import start_end_date, start_end_time
 
 
 class Event(Document):
@@ -17,5 +18,25 @@ class Event(Document):
     detail_url = fields.StringField(required=False)
     venue = fields.StringField(required=False)
     venue_contact = fields.StringField(required=False)
-    venue_location = fields.ListField(fields.FloatField(), require=False)
+    venue_location = fields.ListField(fields.FloatField(), required=False)
 
+    name_start_date = Index().ascending('name').ascending('start_date').unique(True)
+
+    @classmethod
+    def from_json(self, ob):
+        e = dict()
+        start_time, end_time = start_end_time(ob['event_time'])
+        start_date, end_date = start_end_date(ob['event_date'])
+        e['name'] = ob['event_name'].strip()
+        e['start_date'] = start_date
+        if end_date: e['end_date'] = end_date
+        e['start_time'] = start_time
+        if end_time: e['end_time'] = end_time
+        e['venue_id'] = int(ob['venue'].strip())
+        e['detail_description'] = ob['details_text'].strip()
+        e['detail_url'] = ob['details_url'].strip()
+        e['venue'] = ob['event_venue'].strip()
+        e['venue_contact'] = ob['event_contact'].strip()
+        if ob['coords']:
+            e['venue_location'] = map(float, list(eval(ob['coords'])))
+        return self(**e)
